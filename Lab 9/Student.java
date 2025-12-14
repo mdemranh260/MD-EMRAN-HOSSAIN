baf902b6d9c2eb5827b790f165ce280ef42b1d40
mdemranh260 <mdemranh260@gmail.com>
2025-12-14 20:37:20 +0600

    Create Student.java

---
+++ Lab 9/Student.java
@@ -0,0 +1,41 @@
+class Student {
+    private String name;
+    private String id;
+    private double cgpa;
+    // Default Constructor
+    public Student() {
+    }
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setCgpa(double cgpa) {
+        this.cgpa = cgpa;
+    }
+    // Getters
+    public String getName() {
+        return name;
+    }
+    public String getId() {
+        return id;
+    }
+
+    public double getCgpa() {
+        return cgpa;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.setName("MD EMRAN HOSSAIN");
+         s.setId("E251004");
+        s.setCgpa(4.00)
+        System.out.println("Student Information:");
+        System.out.println("Name: " + s.getName());
+        System.out.println("ID: " + s.getId());
+        System.out.println("CGPA: " + s.getCgpa());
+    }
+}
